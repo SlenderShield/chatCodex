8843fd45b6061fb75b653b1efe3926d7fb912d7c
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2022-12-23 23:32:53 +0530

    Initial Commit

---
+++ client/script.js
@@ -0,0 +1,105 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+// Loading when data is retrieved
+function loader(element) {
+  element.textContent = "";
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+// Bot typing function
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+// id for every message
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexaDecimal = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexaDecimal}`;
+}
+
+function chatStrip(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && 'ai'}">
+    <div class="chat">
+    <div class="profile">
+    <img src=${isAi ? bot : user} alt="${isAi ? "bot" : "user"}">
+    </div>
+    <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // User chat
+  chatContainer.innerHTML += chatStrip(false, data.get('prompt'))
+  form.reset();
+
+  // Bot chat
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStrip(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+  // fetch data from server ->bot's response
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parseData = data.bot.trim();
+
+    typeText(messageDiv, parseData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong"
+    alert(err)
+  }
+}
+
+// form submit
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode == 13) {
+    handleSubmit(e)
+  }
+})
